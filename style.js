61837ea9de20d7a74889619ffa218869f53530c0
Yulia Krimerman <juliapiterova@hotmail.com>
2019-02-27 12:05:49 -0500

    changes in Css

---
+++ style.js
@@ -0,0 +1,20 @@
+$(".scroll").on("click", function( e ) {
+    
+    e.preventDefault();
+
+    $("body, html").animate({ 
+        scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+    
+});
+
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+
+
